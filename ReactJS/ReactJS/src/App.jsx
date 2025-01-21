f549d5292ce81d2e9f6f81ab58563a61631e4a26
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2025-01-20 23:36:08 -0300

    Se agrego links de navegacion

--- ReactJS/ReactJS/src/App.jsx
+++ ReactJS/ReactJS/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// Componentes 
 import NavBar from './components/NavBar/NavBar';
 import CartWidget from './components/CartWidget/CartWidget';  
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
+// Componentes adicionales para las rutas
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   const remera = {
@@ -10,11 +17,25 @@ const App = () => {
   };
 
   return (
-    <>
+    <Router>
+      {/* Aquí tienes los enlaces de navegación */}
       <NavBar />
       <CartWidget />  
-      <ItemListContainer/>
-    </>
+
+      {/* Links de navegación */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/productos">Productos</Link></li>
+        </ul>
+      </nav>
+
+      {/* Aquí renderizas los componentes dependiendo de la ruta */}
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/productos" component={ItemListContainer} />
+    </Router>
   );
 };
 
